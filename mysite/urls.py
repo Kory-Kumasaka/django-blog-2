c71168cedd1fb908ce5fb507cd405f047ec690c3
Kory Kumasaka <korykumasaka@gmail.com>
2020-05-24 13:49:01 -0700

    Completed tutorial, issue with the base template call to social log in, instead of redirecting to social log in root directory (blogging list) is retunred

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,9 +19,11 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path('', include('blogging.urls')),
-    path('polling/', include('polling.urls')),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+    path('accounts/', include('allauth.urls')),
+    path('', include('blogging.urls')),
+    path('polling/', include('polling.urls')),
+
 ]
